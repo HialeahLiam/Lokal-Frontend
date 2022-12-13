e1cce818b360305b6bafe78b13127fa2b471e3f1
Liam Idrovo <liamidrovo@gmail.com>
2022-12-13 18:33:57 -0500

    Spotify Embed
    Created page to experiment with Spotify embeds.
    Will play full tracks if you are logged in to Spotify on your browser.
    Will play previews otherwise

---
+++ app/spotify-embed/page.js
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react'
+
+function Page() {
+  return (
+    <div>
+        <iframe style={{borderRadius: '12px'}} src="https://open.spotify.com/embed/track/2mDYYGaGd9uXKkK2YhDA3i?utm_source=generator" width="50%" height="80px" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        {/* <link rel="alternate" type="application/json+oembed" href="https://open.spotify.com/oembed?url=https%3A%2F%2Fopen.spotify.com%2Fshow%2F5eXZwvvxt3K2dxha3BSaAe" /> */}
+    </div>
+  )
+}
+
+export default Page
