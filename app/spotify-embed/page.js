900d233a7a6e70a2092a67c16661a7eddef617a4
Liam Idrovo <liamidrovo@gmail.com>
2022-12-13 19:14:29 -0500

    Resized Spotify embed to check on mobile

--- app/spotify-embed/page.js
+++ app/spotify-embed/page.js
@@ -4,10 +4,13 @@ import React from 'react'
 function Page() {
   return (
     <div>
-        <iframe style={{borderRadius: '12px'}} src="https://open.spotify.com/embed/track/2mDYYGaGd9uXKkK2YhDA3i?utm_source=generator" width="50%" height="80px" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        <iframe style={{borderRadius: '12px'}} src="https://open.spotify.com/embed/album/2xkZV2Hl1Omi8rk2D7t5lN?utm_source=generator" width="90%" height="180" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        
         {/* <link rel="alternate" type="application/json+oembed" href="https://open.spotify.com/oembed?url=https%3A%2F%2Fopen.spotify.com%2Fshow%2F5eXZwvvxt3K2dxha3BSaAe" /> */}
     </div>
   )
 }
+
+
 
 export default Page
